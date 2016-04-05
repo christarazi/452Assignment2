8b20e356cf57215190381c48ef6314fa209c07b6
Chris Tarazi <chris.tarazi@yahoo.com>
2016-04-05 09:38:46 -0700

    Ready for branching; Update readme and add all skeleton files

--- README.md
+++ README.md
@@ -7,3 +7,18 @@ C++ program for CPSC 452 assignment 2.
  - Austin Greene
 
 ## Instructions
+
+```shell
+$ make clean	# Cleans up all files from previous build.
+$ make 			# Builds the entire project.
+$ ./cipher 		# Runs the program.
+```
+
+## Contributing
+
+Please make sure that all changes you make are made first, in your own branch, then we can merge them in after review. 
+
+To make a new branch:
+```shell
+$ git checkout -b <new_branch>
+```
---
+++ RSA.cpp
@@ -0,0 +1,34 @@
+
+#include "RSA.h"
+
+/**
+ * Sets the key to use
+ * @param key - the key to use
+ * @return - True if the key is valid and False otherwise
+ */
+bool RSA_433::setKey(const unsigned char* key)
+{
+	return false;
+}
+
+/**	
+ * Encrypts a plaintext string
+ * @param plaintext - the plaintext string
+ * @return - the encrypted ciphertext string
+ */
+unsigned char* RSA_433::encrypt(const unsigned char* plaintext)
+{
+	
+	return NULL;
+}
+
+/**
+ * Decrypts a string of ciphertext
+ * @param ciphertext - the ciphertext
+ * @return - the plaintext
+ */
+unsigned char* RSA_433::decrypt(const unsigned char* ciphertext)
+{
+	return NULL;
+}
+
